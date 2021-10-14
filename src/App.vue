94531b37b87fb3f9dea8a5c41a85cccbb28ac0ff
mafromist <mafromist@gmail.com>
2021-10-14 23:53:39 +0300

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,114 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>Musix Player</h1>
+    </header>
+
+    <main>
+      <section class="player">
+        <h2 class="song-title">{{ current.title }} - <span>{{ current.artist }}</span></h2>
+        <div class="controls">
+          <button class="prev" @click="prev">Prev</button>
+          <button class="play" v-if="!isPlaying" @click="play">Play</button>
+          <button class="pause" v-else @click="pause">Pause</button>
+          <button class="next" @click="next">Next</button>
+        </div>
+      </section>
+      <section class="playlist">
+        <h3>Playlist</h3>
+        <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src) == (current.src) ? 'song playing' : 'song' ">
+          {{ song.title }} - {{ song.artist }}
+        </button>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      current: {},
+      index: 0,
+      isPlaying: false,
+      songs: [
+        {
+          title: 'Love of My Life',
+          artist: 'Ben Lvcas',
+          src: require('../src/assets/Ben_Lvcas_-_Love_of_My_Life.mp3')
+        },
+        {
+          title: 'Disconnect',
+          artist: 'The Vow',
+          src: require('../src/assets/The_Vow_-_Disconnect.mp3')
+        }
+      ],
+      player: new Audio(),
+    }
+  },
+  methods: {
+    play(song) {
+      if (typeof song.src != "undefined") {
+        this.current = song;
+        this.player.src = this.current.src;
+      }
+       this.player.play();
+       this.player.addEventListener("ended", function () {
+        this.index++;
+        if(this.index > this.songs.index) {
+          this.index = 0;
+          }
+          this.current = this.songs[this.index];
+          this.play(this.current);
+       }.bind(this));
+       this.isPlaying = true;
+    },
+    pause() {
+      this.player.pause();
+      this.isPlaying = false;
+    },
+    next() {
+      this.index++;
+      if(this.index > this.songs.index) {
+        this.index = 0;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    },
+    prev() {
+      this.index--;
+      if(this.index < 0) {
+        this.index = this.songs.index - 1;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    }
+  },
+  created () {
+    this.current = this.songs[this.index];
+    this.player.src = this.current.src;
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  }
+
+  header {
+    display: grid;
+    place-items: center;
+    padding: 20px;
+    background-color: rgb(63, 26, 13);
+    color: #eff6d8;
+  }
 </style>
